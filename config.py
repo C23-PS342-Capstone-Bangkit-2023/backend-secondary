6d1f9f68bf39c5c6357361434750d3119b130fbc
Bayu Setiawan <warnaf09@gmail.com>
2023-06-07 16:54:15 +0700

    fix

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ from flaskext.mysql import MySQL
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
-app.config['MYSQL_DATABASE_DB'] = 'cp_2023'
+app.config['MYSQL_DATABASE_DB'] = 'test_ml_data'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
--- main.py
+++ main.py
@@ -17,47 +17,18 @@ model = joblib.load('knn_model.pkl')
 def recommend():
     try:
         _json = request.json
-        _namaMakanan = _json['name']
-        conn = mysql.connect()
-        cursor = conn.cursor(pymysql.cursors.DictCursor)
-        cursor.execute("SELECT meal_id, meal_name, meal_image, calories, carb, protein, fat, tag FROM meals_data")
-        empRows = cursor.fetchall()
-        changeJson = jsonify(empRows)
-        respone = changeJson
+        _namaMakanan = _json['data']
+        print(_namaMakanan)
         # running ml 1
-        # df = pd.DataFrame(empRows, columns = ['meal_id', 'meal_name', 'meal_image', 'calories', 'carb', 'protein', 'fat', 'tag'])
-        # user_query_embedding = tfidf_model.transform([_namaMakanan]).toarray()
-        # distances, indices = model.kneighbors(user_query_embedding)
-        # top_recommended_items = df.iloc[indices[0]]
-        # print(df)
-        # print(user_query_embedding)
-        # print(distances)
-        # print(indices)
-        # print(top_recommended_items)
-
-        # running ml 2
-        # df = pd.DataFrame(empRows, columns = ['tag'])
-        # tf_idf_tag = tfidf_model.fit_transform(df)
-        # to_array = [v.toarray() for v in tf_idf_tag]
-        # tf_idf_tag_array = np.concatenate(to_array, axis=0)
-
-        # model = NearestNeighbors(n_neighbors=4)
-        # model.fit(tf_idf_tag_array)
-
-        # user_input = ['vegan', 'karbohidrat', 'protein']
-        # user_input_tf_idf = tfidf_model.transform(user_input).toarray()
-        # distances, indices = model.kneighbors(user_input_tf_idf)
-        # top_10_food = df.iloc[indices[0]]
-        tmpCall = pd.read_json(changeJson)
-        print(tmpCall)
-        respone.status_code = 200
-        return respone
-        # return jsonify(top_recommended_items.to_dict(orient='records'))
+        df = pd.read_csv('recommend.csv')
+        user_query_embedding = tfidf_model.transform(_namaMakanan).toarray()
+        distances, indices = model.kneighbors(user_query_embedding)
+        top_recommended_items = df.iloc[indices[0]]
+        response = { "data": top_recommended_items.to_dict(orient='records')}
+        return jsonify(response)
     except Exception as e:
         print(e)
-    finally:
-        cursor.close()
-        conn.close()
+        return "a string"
 
 @app.errorhandler(404)
 def showMessage(error=None):
